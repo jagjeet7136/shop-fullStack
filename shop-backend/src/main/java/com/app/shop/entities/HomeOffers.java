c18a104362b361d51a577f495509fc50b34fcfca
Jagjeet Singh <jagjeet7136@gmail.com>
2024-05-17 20:17:51 -0400

    upgrading project to 3.1.11

--- shop-backend/src/main/java/com/app/shop/entities/HomeOffers.java
+++ shop-backend/src/main/java/com/app/shop/entities/HomeOffers.java
@@ -1,10 +1,10 @@
 package com.app.shop.entities;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.DynamicUpdate;
-import javax.persistence.*;
 
 @Entity
 @Getter
