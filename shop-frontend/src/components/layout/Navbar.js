358932e30b86d4866d6e9ef65a7cbea2cb954a96
Jagjeet Singh <jagjeet7136@gmail.com>
2024-07-04 15:03:52 -0400

    Navbar Design Completed Two Media Queries

--- shop-frontend/src/components/layout/Navbar.js
+++ shop-frontend/src/components/layout/Navbar.js
@@ -7,24 +7,30 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
     return (
         <nav>
-            <div className={styles.logoSearchContainer}>
-                <a href="/" className={styles.logo}><img src={logo} alt="logo" /></a>
-                <div className={styles.search}>
-                    <input type="text" placeholder="Search..." />
-                    <Link><i className="fa fa-search"></i></Link>
+            <div className={styles.nav}>
+                <div className={styles.logoSearchContainer}>
+                    <a href="/" className={styles.logo}><img src={logo} alt="logo" /></a>
+                    <div className={styles.search}>
+                        <input type="text" placeholder="Search..." />
+                        <Link><i className="fa fa-search"></i></Link>
+                    </div>
+                </div>
+                <div className={styles.cartAccountContainer}>
+                    <Link className={styles.account}>
+                        <i className="fa fa-search"></i>
+                        <span>Account</span>
+                    </Link>
+                    <Link className={styles.cart}>
+                        <div className={styles.cartLogoContainer}>
+                            <i className="fa fa-shopping-cart"></i>
+                        </div>
+                        <span>Cart</span>
+                    </Link>
                 </div>
             </div>
-            <div className={styles.cartAccountContainer}>
-                <Link className={styles.account}>
-                    <i className="fa fa-search"></i>
-                    <span>Account</span>
-                </Link>
-                <Link className={styles.cart}>
-                    <div className={styles.cartLogoContainer}>
-                        <i className="fa fa-shopping-cart"></i>
-                    </div>
-                    <span>Cart</span>
-                </Link>
+            <div className={styles.secondarySearch}>
+                <input type="text" placeholder="Search..." />
+                <Link><i className="fa fa-search"></i></Link>
             </div>
         </nav>
     );
